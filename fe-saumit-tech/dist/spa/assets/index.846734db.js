1612834695e569d96e8fbf3f9cfa5cbd799af1a4
Sumit Joshi <sumitmumbai007@gmail.com>
2024-12-01 13:17:57 +0530

    Mobile build

--- fe-saumit-tech/dist/spa/assets/index.846734db.js
+++ fe-saumit-tech/dist/spa/assets/index.846734db.js
@@ -1,4 +1,4 @@
-import{d as jt,c as E,w as un,h as ke}from"./index.400c94f2.js";/*!
+import{d as jt,c as E,w as un,h as ke}from"./index.1c3eb647.js";/*!
  * Font Awesome Free 6.7.1 by @fontawesome - https://fontawesome.com
  * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
  * Copyright 2024 Fonticons, Inc.
